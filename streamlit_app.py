c2d3b0d76b5cab3ebc2213225ef8fddbd3e92ab6
AmigoLoco25 <aifs@wio.eco>
2025-07-14 15:15:57 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -118,7 +118,7 @@ def get_products_info_for_row(row_idx, df_presupuesto, product_lookup):
                 fondo = value
 
         if net_w is None:
-            net_w = info.get("Net Weight")
+            net_w = item.get("weight") or info.get("Net Weight")
 
         if None not in (ancho, alto, fondo):
             volume = round((ancho * alto * fondo) / 1_000_000, 5)
