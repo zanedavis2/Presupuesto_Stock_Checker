2e3a96ac314935db879bba24b1df236e59f159a1
AmigoLoco25 <aifs@wio.eco>
2025-07-17 10:36:44 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -211,7 +211,7 @@ def get_products_info_for_row(row_idx, df_presupuesto, product_lookup):
         "Units", "Subtotal > Units" ,"Net Weight (kg)","Total Weight (kg)", "Subtotal > Total Weight (kg)",
         "Volume (m³)", "Subtotal > Volume (m³)","Stock Disponible","Insuficiente?","Falta", "Subtotal > Falta"
     ]
-   for col in header_cols:
+    for col in header_cols:
         df.loc[header_mask, col] = df.loc[header_mask, col].fillna(" ")
 
 
